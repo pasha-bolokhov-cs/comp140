ee2d5743c0b5f69241ba0a5662a9613c162c9fef
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2014-10-16 14:55:14 -0700

    Add initial version of 'lab4'

---
+++ lab4/js/main.js
@@ -0,0 +1,36 @@
+/**
+ * AngularJS Tutorial 1
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ */
+
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('tutorialWebApp', [
+  'ngRoute'
+]);
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    .when("/home", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    // Pages
+    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    .when("/menu", {templateUrl: "partials/menu.html", controller: "PageCtrl"})
+    .when("/specials", {templateUrl: "partials/specials.html", controller: "PageCtrl"})
+    .when("/opportunity", {templateUrl: "partials/opportunity.html", controller: "PageCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
+});
