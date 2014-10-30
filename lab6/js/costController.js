325556ce45084b99d21ff850c2c6bf9b0fab3ffb
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2014-10-30 15:22:50 -0700

    Edit controllers

---
+++ lab6/js/costController.js
@@ -0,0 +1,12 @@
+/*
+ * Control the price
+ */
+function costController($scope)
+{
+    $scope.total = 29.98;
+    $scope.calculateTotal() = 
+	function() {
+	return 49.99;
+    }
+}
+
--- lab6/js/main.js
+++ lab6/js/main.js
@@ -33,14 +33,3 @@ app.controller('PageCtrl', function (/* $scope, $location, $http */) {
 
 });
 
-/*
- * Control the price
- */
-function costController($scope)
-{
-    $scope.total = 29.98;
-    $scope.calculateTotal() = 
-	function() {
-	return 49.99;
-    }
-}
