97e426b22e9f9f0125689bda566f51feb3538270
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2014-10-30 10:28:40 -0700

    Prepare 'submit.php' properly

--- lab5/php/submit.php
+++ lab5/php/submit.php
@@ -3,196 +3,141 @@
 <head>
 
 <title>
-  Form results
+   Order Details
 </title>
 <style type="text/css">
-
-.formval { font-size:14px;
-		   font-weight: 900;
-		   font-family:Verdana, Arial, Helvetica, sans-serif;
-           color: blue;
-         }
+   .formval { 
+   font-size: 14px;
+   font-weight: 900;
+   font-family: Verdana, Arial, Helvetica, sans-serif;
+   color: blue;
+   }
 </style>
 </head>
 <body>
-  <h1>Form data entered by client</h1>
+  <h1>Order Data provided</h1>
 
 <?php
 
 
 date_default_timezone_set('America/Vancouver');
 
-	/* In PHP the assignment operator is the equals sign.  */
 
-	/* PHP variable names appear on the left of the equals sign */
+/* The form element values are assigned to the PHP variables below.   */
 
-	/* The $_REQUEST is one of the PHP predefined variables.
+$firstname  = $_REQUEST['firstname'   ];   /* text box   */
+$lastname   = $_REQUEST['lastname'    ];   /* text box   */
+$phone      = $_REQUEST['phone'       ];   /* radio        */
+$credit     = $_REQUEST['credit'      ];   /* checkbox */
+$expirym    = $_REQUEST['expirym'     ];   /* select       */
+$expiryy    = $_REQUEST['expiryy'     ];   /* select       */
+$serving    = $_REQUEST['serving'     ];
+$flavour    = $_REQUEST['flavour'     ];
+if (isset($_REQUEST[ 'spice' ])) {
+  $spice    = $_REQUEST[ 'spice'      ];
+} else {
+  $spice = array();
+}
+$quantity   = $_REQUEST[ 'quantity'   ];
+$order      = $_REQUEST[ 'order'      ];
 
-	    Through $_REQUEST the form information is passed more securely to
-the
-               PHP script handler (like this one).
 
-	     http://ca.php.net/manual/en/language.variables.predefined.php
+/* Information for using dates in PHP is at  http://ca3.php.net/date */
+$date = date('D j M Y G:i');
 
-               Other variables such as $_GET and $POST are no longer used
-because they
-               cause the form information to appear in the URL to the PHP
-script.
+/* Check if $firstname was not entered */
+if (empty($firstname)) {
+  echo "<p><strong>You didn't enter a value for first name." .
+    "<br />" .
+    "Please <a href=\"../index.html#/order\">go back</a> and re-enter a value for first name." .
+    "</strong></p></body></html>";
+  exit;
+}
 
-           */
-	/* The names appearing within the $_REQUEST[  ] are the
-	      form element names. */
+echo "<p>Order processed<br />";
 
-    /* The form element values are assigned to the PHP variables below.   */
+echo "The current time and date is " . date("H:i, jS F");
 
-    $firstname  = $_REQUEST[ 'firstname'  ];   /* text box   */
-    $lastname   = $_REQUEST[ 'lastname'   ];   /* text box   */
-    $phone      = $_REQUEST[ 'phone'     ];   /* radio        */
-    $credit     = $_REQUEST[ 'credit'      ];   /* checkbox */
-    $expiry     = $_REQUEST[ 'expiry'    ];   /* select       */
-    $pizza      = $_REQUEST[ 'pizza'     ];
-    $size       = $_REQUEST[ 'size'      ];
-    if ( isset( $_REQUEST[ 'topping' ] ) ){
-                $topping    = $_REQUEST[ 'topping'   ];
-    } else {
-         $topping = array();
+echo "<br />";
+
+/* Show the entered form information back to the browser window */
+echo "First name is <span class=\"formval\">" . $firstname  . "</span><br />";
+
+if ($lastname)
+  echo "Last name is <span class=\"formval\">"  . $lastname  . "</span><br />";
+
+if ($phone )
+  echo "phone is <span class=\"formval\">" . $phone . "</span><br />";
+
+if ($credit)
+  echo "Credit card is <span class=\"formval\">" . $credit . "</span><br/>";
+
+if ($expirym && $expiryy)
+  echo "Expiry is <span class=\"formval\">" . $expirym . " / " . $expiryy . "</span><br />";
+
+if ($flavour)
+  echo "Sushi flavour is <span class=\"formval\">" . $flavour . "</span><br/>";
+
+if ($serving)
+  echo "Sushi package serving is <span class=\"formval\">" . $serving . "</span><br/>";
+
+
+if (count($spice) > 0) {
+  echo "Selected spice(s) is/are <span class=\"formval\">";
+  
+  foreach($spice as $key => $value) {
+    echo $value . " ";
+  }
+  
+  echo "</span><br />";
+}
+
+echo "Quantity is <span class='formval'>" . $quantity    . "</span><br/>";
+
+echo "Order is <span class='formval'>" . $order  . "</span><br />";
+
+
+/* 
+ * Write the information to the file myOrders.txt, which should have
+ * write permission to "others".  First check if the orders file can
+ * be opened with append.
+ */
+@ $fp = fopen("./orders.txt", "a");
+if (!$fp) {
+  echo "<p><strong>Your order cannot be processed at this time. " .
+    "Please try again later.</strong> <br />" .
+    "<a href=\"../index.html#/order\">Return to the order page</a>" .
+    "</p></body></html>";
+  exit;
+}
+
+
+$outputString = 
+  $date       . "\t" .
+  $firstname  . "\t" .
+  $lastname   . "\t" .
+  $credit     . "\t" ;
+
+if (count($spice) > 0) {
+
+  foreach($spice as $key => $value) {
+    $outputString .= $value . "\t";
     }
-    $quantity   = $_REQUEST[ 'quantity'  ];
-    $order      = $_REQUEST[ 'order'     ];
+}
 
+$outputString .= $flavour . "\n";
 
-	/* Information for using dates in PHP is at  http://ca3.php.net/date
- * */
-	$date = date('D j M Y G:i');
+fwrite($fp, $outputString);
 
-    /* Check if the $firstname was not entered on the form.  Note that \" is a
- * way of providing double quotes
-	    inside double quotes.
-          */
-      if ( empty($firstname) ) {
+fclose($fp);
 
-  	     echo "<p><strong>You didn't enter a value for first name." .
-              "<br />" .
-              "Please <a href=\"order.html\">go back</a> and re-enter a value for first name." .
-              "</strong></p></body></html>";
-         exit;
-      }
-
-      echo "<p>Order processed<br />";
-
-      echo "The current time and date is " . date("H:i, jS F");
-
-  echo "<br />";
-
-      /* Show the entered form information back to the browser window.
-                     If the form has differently named form elements, you will
-have
-                    to change them in this PHP file.
-                */
-      echo "First name is <span class=\"formval\">" . $firstname  .
-"</span><br />";
-
-	  if ( $lastname)
-		echo "Last name is <span class=\"formval\">"  . $lastname   .
-"</span><br />";
-
-	  if ( $phone )
-        echo "phone is <span class=\"formval\">" . $phone . "</span><br />";
-
-	  if ( $credit )
-        echo "Credit card is <span class=\"formval\">" . $credit . "</span><br
-/>";
-
-	  if ( $expiry )
-        echo "Expiry is <span class=\"formval\">" . $expiry . "</span><br />";
-
-	  if ( $pizza )
-        echo "Pizza type is <span class=\"formval\">" . $pizza . "</span><br
-/>";
-
-	  if ( $size )
-        echo "Pizza size is <span class=\"formval\">" . $size . "</span><br
-/>";
-
-
-      if ( count( $topping) > 0 ) {
-        echo "Selected toppings is/are <span class=\"formval\">";
-
-        foreach($topping as $key => $value)
-        {
-          echo $value . " ";
-        }
-
-        echo "</span><br />";
-
-      }
-      echo "Quantity is <span class='formval'>" . $quantity    . "</span><br
-/>";
-
-      echo "Order is <span class='formval'>" . $order  . "</span><br />";
-
-
-      /* Write the information to the file myOrders.txt, which should have
-                    write permission to "others".  First check if the orders
-file can
-	          be opened with append.
-               */
-      @ $fp = fopen("./myOrders.txt", "a");
-      if (!$fp)
-      {
-         echo "<p><strong>Your order cannot be processed at this time. " .
-           "Please try again later.</strong> <br />" .
-		   "<a href=\"order.html\">Return to the form</a>" .
-		   "</p></body></html>";
-         exit;
-      }
-
-	  /* $outputString is assigned the information you entered on the form
- * separated by tabs (the \t means tab)  */
-
-	  /* For any form information not entered, the associated PHP variable
- * will contain nothing (blank).  */
-
-	  /*  The pizzaTop[0] contains the first checkbox form element value
- *  or blank if it wasn't checked.   */
-
-	  /*  The \n is necessary at the very end of the string so that each
- *  record starts on a new line.  */
-
-      $outputString = $date       . "\t" .
-                      $firstname  . "\t" .
-                      $lastname   . "\t" .
-		              $credit     . "\t" ;
-
-      if ( count( $topping) > 0 ) {
-
-        foreach($topping as $key => $value)
-        {
-          $outputString .= $value  . "\t";
-        }
-      }
-
-      $outputString .=
-                      $pizza   .
-                      "\n";
-
-	  /*  Write out the form information through the file pointer to the
- *  file myOrders.txt.  */
-      fwrite($fp, $outputString);
-
-	  /* Close the file pointer now that we are finished working with the
- * file.  */
-      fclose($fp);
-
-	  /*  PHP can include an e-mail function so that you may optionally
- *  email the form information. */
-
-        // imap_mail("zz@shaw.ca", "Test Message", $outputString,
-        // "From:Ghostly");
-
+/* 
+ * PHP can include an e-mail function so that you may optionally
+ * email the form information
+ */
 ?>
 <p>
- <a href="../order.html">Return to the form</a>
+ <a href="../index.html#/order">Return to the order page</a>
 </p>
 </body>
 </html>
