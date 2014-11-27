c6bb7b7014ddd69c823bd22ef769cb587a9fa3ea
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2014-11-27 09:58:06 -0800

    Remove "Order" from navigation bar

--- lab7/js/main.js
+++ lab7/js/main.js
@@ -21,9 +21,6 @@ app.config(['$routeProvider', function ($routeProvider) {
     // Pages
     .when("/about",       {templateUrl: "partials/about.html", controller: "PageCtrl"})
     .when("/menu",        {templateUrl: "partials/menu.html", controller: "PageCtrl"})
-    .when("/specials",    {templateUrl: "partials/specials.html", controller: "PageCtrl"})
-    .when("/order",       {templateUrl: "partials/order.html", controller: "PageCtrl"})
-    .when("/opportunity", {templateUrl: "partials/opportunity.html", controller: "PageCtrl"})
     // else 404
     .otherwise("/404",    {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
